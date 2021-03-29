35349354c4978ad5e813eed5ee048a7687b60dc6
Woodhouse <bwoodhouse@corp.arete.com>
2021-03-29 11:43:17 -0700

    Setting up skeleton

---
+++ controller.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 11:39:03 2021
+
+@author: bwoodhouse
+
+Maintain game state while playing Gwent and take automatic actions.
+
+Initial goals:
+Automatically play a game.
+Random mulligan, random plays (of trivial deck to play).
+Pass if opponent passes and you are ahead in score, otherwise play to random location.
+"""
+
+def initialize():
+    pass
+
+if __name__ == "__main__":
+    initialize()
