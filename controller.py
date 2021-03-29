7c4f2e94bfab7686019a216312b38f97b0b8cf26
Woodhouse <bwoodhouse@corp.arete.com>
2021-03-29 11:58:15 -0700

    Added more skeleton

--- controller.py
+++ controller.py
@@ -12,8 +12,36 @@ Random mulligan, random plays (of trivial deck to play).
 Pass if opponent passes and you are ahead in score, otherwise play to random location.
 """
 
-def initialize():
+import time
+
+def analyze_game_state():
+    # observe and record everything possible about the current game state
+    pass
+
+def end_game():
+    # click in a few places to move back to primary menu
+    pass
+
+def initialize_game():
+    # click to launch a game
+    # wait until mulligan screen
+    pass
+
+def make_move():
+    # follow a specified behavior pattern and click appropriately to play a card
     pass
 
 if __name__ == "__main__":
-    initialize()
+    # recognize menu screen where a game can be launched
+    # play unranked for now
+    # infinite loop to keep playing more games
+    while True:
+        initialize_game()
+        game_active = True
+        my_turn = True
+        while game_active:
+            analyze_game_state()
+            if my_turn:
+                make_move()
+                time.sleep(2)
+        end_game()
