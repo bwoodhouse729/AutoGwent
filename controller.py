5d277f4acbe24d35a8cc274d09b3b2fbc54a4306
Brent Woodhouse <bwoodhouse729@gmail.com>
2021-04-01 15:55:01 -0700

    Successfully reached mulligan screen

--- controller.py
+++ controller.py
@@ -14,15 +14,16 @@ Pass if opponent passes and you are ahead in score, otherwise play to random loc
 TODO: Take a few screenshots of each type of screen for development purposes.
 """
 
+import imageio
+from matplotlib import pyplot as plt
+import numpy as np
 import pyautogui
 import time
 
 def analyze_game_state():
     # observe and record everything possible about the current game state
     
-    # Screenshot the whole game window and save it out
-    myScreenshot = pyautogui.screenshot(region=(0, 31, 1600, 900))
-    myScreenshot.save('./screenshots/active_screen.png')
+    take_screenshot()
     
     # Identify the current game window
     board_active = check_for_board()
@@ -61,7 +62,6 @@ def analyze_game_state():
     
     # TODO: If opponent is playing a card, record it
     # Recognizing more detailed opponent actions would be quite tricky (and not that helpful?)
-    
 
 def check_for_board():
     # return True if currently viewing the game board
@@ -106,6 +106,51 @@ def make_move():
     
     pass
 
+def make_mulligans():
+    # identify and click cards for mulligan
+    
+    # TODO: Load test image to isolate cards
+    
+    # TODO: Identify cards by comparing to saved images
+    
+    # TODO: Make sure execution is < 3 seconds total
+    
+    # TODO: Randomly select card to mulligan at this point
+    
+    pass
+
+def take_screenshot():
+    # Screenshot the whole game window and save it out
+    myScreenshot = pyautogui.screenshot(region=(0, 31, 1600, 900))
+    myScreenshot.save('./screenshots/active_screen.png')
+
+def transition_game_select_play_standard():
+    pyautogui.dragTo(378, 473, 0.1)
+    pyautogui.click()
+    
+    # wait until game starts - look for mulligan screen
+    # take screenshot every 2 seconds until pixel at (323, 405) is blue-green
+    # blue-green is [48, 255, 255]
+    attempts = 90
+    threshold = 10
+    for i in range(attempts):
+        take_screenshot()
+        image = imageio.imread('./screenshots/active_screen.png')
+        
+        # plt.imshow(image[320:330, 400:410])
+        # plt.show()
+        
+        if (np.linalg.norm(image[323, 405] - [48, 255, 255]) < threshold):
+            #print('found')
+            break
+        
+        time.sleep(2)
+
+def transition_home_game_select():
+    pyautogui.dragTo(814, 472, 0.1)
+    pyautogui.click()
+    time.sleep(2)
+
 if __name__ == "__main__":
     # recognize menu screen where a game can be launched
     # TODO: use check_for_game_select
@@ -114,18 +159,29 @@ if __name__ == "__main__":
     # pause to allow user to make Gwent window active
     time.sleep(3)
     
+    #take_screenshot()
+    
+    # transition_home_game_select()
+    # transition_game_select_play_standard()
+    
+    make_mulligans()
+    
+    # image = imageio.imread('./development_screenshots/sample_start_of_game.png')
+    # print(image[323, 405])
+        
+    
     # infinite loop to keep playing more games
-    while True:
-        initialize_game()
-        game_active = True
-        my_turn = True
-        while game_active:
-            analyze_game_state()
-            if my_turn:
-                make_move()
-                time.sleep(1)
+    # while True:
+    #     initialize_game()
+    #     game_active = True
+    #     my_turn = True
+    #     while game_active:
+    #         analyze_game_state()
+    #         if my_turn:
+    #             make_move()
+    #             time.sleep(1)
                 
-            # for testing purposes
-            game_active = False
-        end_game()
-        break
+    #         # for testing purposes
+    #         game_active = False
+    #     end_game()
+    #     break
