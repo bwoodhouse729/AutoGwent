3bd1968b975a593efcfb17d7b5ff2a00efe710df
Brent Woodhouse <bwoodhouse729@gmail.com>
2021-04-10 21:36:38 -0700

    Comments

--- controller.py
+++ controller.py
@@ -261,9 +261,11 @@ def identify_allied_hand():
 def identify_board():
     # Identify all cards on the board and their various attributes
     # TODO: Identify cards on each row of the board via their static image
-    # Start by using lower right corner of the cards (bronze or gold) to
+    # Start by using upper right corner of the cards (bronze or gold) to
     # indicate how many cards are present.  Need coordinates for even & odd
     # (take photos of 8 and 9 card rows), all 4 corners.
+    # Note the coordinates depend on whose turn it is.  The camera zooms
+    # out when it is player 0's turn.
     
     # TODO: Recognize the back of cards as well for traps
     # TODO: Identify card power
